fc88e6aa20a519b549b72d8332ec93e483c61d93
kmilo <kmilouh@gmail.com>
2023-09-26 16:50:56 +0200

    remove labels

--- shell_custom/terminal.sh
+++ shell_custom/terminal.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-export DET_MASTER=172.27.1.63:80
+#export DET_MASTER=172.27.1.63:80
 
-export DET_USER=demo
-export DET_PASS=demo
+#export DET_USER=demo
+#export DET_PASS=demo
 
 det shell start --config-file config.yml -w kmilo-demo 
 

15734a8aebb45d6ef42223f1f0709248e2597d92
kmilo <kmilouh@gmail.com>
2023-10-08 18:20:40 +0200

    add sample with caps

---
+++ shell_custom_add_caps/terminal.bat
@@ -0,0 +1,11 @@
+
+set DET_MASTER=172.27.1.63:80
+
+set DET_USER=demo
+set DET_PASS=demo
+
+det shell start --config-file config.yml -w kmilo-demo 
+
+REM det shell open <SHELL UUID> 
+
+REM det shell show_ssh_command <SHELL UUID>
---
+++ shell_custom_add_caps/terminal.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#export DET_MASTER=172.27.1.63:80
+
+#export DET_USER=demo
+#export DET_PASS=demo
+
+det shell start --config-file config.yml -w kmilo-demo 
+
+#det shell open <SHELL UUID> 
+
+# det shell show_ssh_command <SHELL UUID>

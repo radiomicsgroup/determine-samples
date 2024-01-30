738c59dabda729ff90f29e1322a6cdb71d3e0c17
Camilo <kmilouh@gmail.com>
2024-01-30 13:14:33 +0100

    add chmod +x to CMD commands

--- cmd_custom/terminal.sh
+++ cmd_custom/terminal.sh
@@ -5,4 +5,4 @@ export DET_MASTER=172.27.1.63:80
 export DET_USER=demo
 export DET_PASS=demo
 
-det cmd run --config-file config.yml -c context -w kmilo-demo  "./startupscript.sh"
+det cmd run --config-file config.yml -c context -w kmilo-demo  "chmod +x startupscript.sh && ./startupscript.sh"

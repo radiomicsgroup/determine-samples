9e1faa30c956180c551eaa0a935c8d4c983cd24f
Camilo <kmilouh@gmail.com>
2024-02-09 11:31:44 +0100

    update with open url in bash

--- shell_custom_vscode/context/vscodeweb.sh
+++ shell_custom_vscode/context/vscodeweb.sh
@@ -2,7 +2,16 @@
 echo "Waiting 1 seconds to start ...."
 # HOMECONFIG=/nfs/home/demo/config
 sleep 1
-mkdir -p $HOMECONFIG/$DET_USER/data
-mkdir -p $HOMECONFIG/$DET_USER/extensions
-code-server --bind-addr 0.0.0.0:8080 --disable-telemetry --user-data-dir $HOMECONFIG/$$DET_USER/data --extensions-dir $HOMECONFIG/$$DET_USER/extensions >> /dev/null &
+
+if [ -f "${HOMECONFIG}/${DET_USER}" ]; then
+    echo "Already exist folder"
+else
+    echo "Creating  ${HOMECONFIG}/${DET_USER} " 
+    mkdir -p ${HOMECONFIG}/${DET_USER}
+    chmod -R 777 ${HOMECONFIG}/${DET_USER}
+fi
+
+mkdir -p ${HOMECONFIG}/${DET_USER}/data
+mkdir -p ${HOMECONFIG}/${DET_USER}/extensions
+code-server --bind-addr 0.0.0.0:8080 --disable-telemetry --user-data-dir ${HOMECONFIG}/${DET_USER}/data --extensions-dir ${HOMECONFIG}/${DET_USER}/extensions >> /dev/null &
 

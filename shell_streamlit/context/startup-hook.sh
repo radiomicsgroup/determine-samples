9317ef85e9406e6b446d58690be0c4a85467609c
Camilo <kmilouh@gmail.com>
2024-02-13 15:39:27 +0100

    some fix

--- shell_streamlit/context/startup-hook.sh
+++ shell_streamlit/context/startup-hook.sh
@@ -2,5 +2,5 @@
 # run another setup like:
 # apt update && apt install jq
 
-# #streamlit run /run/determined/workdir/app.py --verver.port 8502
+# #streamlit run /run/determined/workdir/app.py --server.port 8502
 pip install -r requirements.txt
